27ed833df59fd4bccdffd13c484037e446295948
Lucas Slepetys <lucas.slepetyss@gmail.com>
2023-06-17 15:02:20 +0200

    Code more organized + comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Form } from './Form';
-import { NewItemRow } from './NewItemRow';
+import { Form, ItemsColumn } from './grocery_component';
 import { useState } from 'react';
-import data from './data';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import { nanoid } from 'nanoid';
 
+//!Long way of getting local storage
 // const getLocalStorage = () => {
 //   let list = localStorage.getItem('list');
 //   if (list) {
@@ -18,21 +18,32 @@ import { ToastContainer, toast } from 'react-toastify';
 // };
 
 const App = () => {
+  //Short version of getting items from local storage
+  //If local storage is null it will create an empty list
   const [items, setItems] = useState(
     JSON.parse(localStorage.getItem('list') || '[]')
   );
-  const [newItem, setNewItem] = useState('');
 
+  //Local storage only storages data as strings
+  //Therefore localStorage.setItem creates a key value pair
+  //'list' is the key and items converted to string is the value
   function setLocalState(items) {
     localStorage.setItem('list', JSON.stringify(items));
   }
 
+  //removeItem gets the id from the NewItemRow component
+  //The function filters the dictionary with that id sets items and local storage to the new list
   function removeItem(id) {
     setItems(items.filter((item) => item.id !== id));
     setLocalState(items.filter((item) => item.id !== id));
   }
 
+  //editItem gets the id from the NewItemRow component
+  //The function sets idDone from the dictionary with that specific id according to the state of the checkbox
+  //It then sets items to the new list and the local storage
   function editItem(id) {
+    //newItems maps through all items until it finds the one with the specific id
+    //For the one with a specific ID, it deconstructs its dictionary while simultaneously setting isDone to the opposite value (0 or 1).
     const newItems = items.map((item) => {
       if (item.id === id) {
         const newItem = { ...item, isDone: !item.isDone };
@@ -44,27 +55,31 @@ const App = () => {
     setLocalState(newItems);
   }
 
+  //addNewItem takes the name of the new item being added, and adds it to the items list in both useState and local storage
+  function addNewItem(newItemName) {
+    //newItems is a list with all items from the grocery shopping list + the item item the user has just added
+    //nanoid() creates a unique id for the item
+    //name: newItem sets the name of the item to the one the user typed
+    const newItems = [
+      ...items,
+      {
+        id: nanoid(),
+        name: newItemName,
+        isDone: false,
+      },
+    ];
+    setItems(newItems);
+    setLocalState(newItems);
+  }
+
   return (
     <main className='section-center'>
-      <Form
-        newItem={newItem}
-        setNewItem={setNewItem}
-        setItems={setItems}
-        items={items}
-        setLocalState={setLocalState}
-      />
-      <div className='items'>
-        {items.map((item) => {
-          return (
-            <NewItemRow
-              key={item.id}
-              {...item}
-              removeItem={removeItem}
-              editItem={editItem}
-            />
-          );
-        })}
-      </div>
+      {/* Displays form component responsible for adding new items to the grocery
+      list */}
+      <Form addNewItem={addNewItem} />
+      {/* Displays ItemsColumn responsible for showing all items from the grocery list - each as a row */}
+      <ItemsColumn items={items} removeItem={removeItem} editItem={editItem} />
+      {/* Responsible for showing Toast messages when toast.success/error is selected */}
       <ToastContainer />
     </main>
   );
